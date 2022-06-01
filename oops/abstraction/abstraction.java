17353560e2e1c0633f389d0c041365be9f209a8b
Pramod Pathak <manasip612@gmail.com>
2022-06-01 16:32:13 +0530

    abstraction

--- oops/abstraction/abstraction.java
+++ oops/abstraction/abstraction.java
@@ -1,27 +1,22 @@
 package com.oops.abstraction;
 
-abstract class Animal {
-		  // Abstract method (does not have a body)
-		  public abstract void animalSound();
-		  // Regular method
-		  public void sleep() {
-		    System.out.println("Zzz");
-		  }
-		}
-
-		// Subclass (inherit from Animal)
-		class Pig extends Animal {
-		  public void animalSound() {
-		    // The body of animalSound() is provided here
-		    System.out.println("The pig says: wee wee");
+abstract class beverage {
+	public abstract void beverageType();
+	  public void type() {
+	    System.out.println("Beverage is hot drink");
+	  }
+	}
+	class Tea extends beverage {
+		  public void beverageType() {
+		    System.out.println("Beverage is Tea");
 		  }
 	}
-		class abstraction{
-			public static void main(String[] args) {
-				 Pig myPig = new Pig(); // Create a Pig object
-				    myPig.animalSound();
-				    myPig.sleep();
-			}
+	class abstraction{
+		public static void main(String[] args) {
+			Tea t = new Tea();
+			t.type();
+			t.beverageType();
 		}
+	}
 
 
