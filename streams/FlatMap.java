3d1b7f1542e5976bf423a152744443209a3a4cb4
Pramod Pathak <manasip612@gmail.com>
2022-06-09 15:33:10 +0530

    flatmap

--- streams/FlatMap.java
+++ streams/FlatMap.java
@@ -1,5 +1,19 @@
 package com.streams;
 
-public class FlatMap {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class FlatMap {
+	public static void main(String args[]) {
+
+		  String[][] array = new String[][]{{"a", "b"}, {"c", "d"}, {"e", "f"}};
+
+		  List<String> collect = Stream.of(array)     
+		          .flatMap(Stream::of)                
+		          .filter(x -> !"a".equals(x))       
+		          .collect(Collectors.toList());     
+
+		  collect.forEach(System.out::println);
+	}
 }
